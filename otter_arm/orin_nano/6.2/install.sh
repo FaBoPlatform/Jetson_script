7b1accbba1d4a00d4f5b2ded309def1c0df8f845
akira-sasaki <gclue.akira@gmail.com>
2025-05-05 20:00:57 +0900

    Update conda.

--- otter_arm/orin_nano/6.2/install.sh
+++ otter_arm/orin_nano/6.2/install.sh
@@ -50,7 +50,7 @@ source "$HOME/.bashrc"
 
 eval "$(conda shell.bash hook)"
 
-conda create -y -n robot python=3.10
+conda create -y -n otter python=3.10
 conda activate otter
  
 # ===== OttherARM by FaBo ====
