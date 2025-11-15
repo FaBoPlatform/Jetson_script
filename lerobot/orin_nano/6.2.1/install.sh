687e5846ec05dd7938c14f8c61745b2cbe0e91f1
akira-sasaki <gclue.akira@gmail.com>
2025-11-15 19:32:24 +0900

    Linkwo

--- lerobot/orin_nano/6.2.1/install.sh
+++ lerobot/orin_nano/6.2.1/install.sh
@@ -75,12 +75,12 @@ pip3 uninstall -y torch torchvision || true
 
 # -- Torch 2.8.0
 wget -O "$TMP/torch-2.8.0-cp310-cp310-linux_aarch64.whl" \
-  "https://pypi.jetson-ai-lab.io/jp6/cu126/+f/62a/1beee9f2f1470/torch-2.8.0-cp310-cp310-linux_aarch64.whl"
+  "https://pypi.jetson-ai-lab.io/jp6/cu126/+f/62a/1beee9f2f1470/torch-2.8.0-cp310-cp310-linux_aarch64.whl#sha256=62a1beee9f2f147076a974d2942c90060c12771c94740830327cae705b2595fc"
 python3 -m pip install "$TMP/torch-2.8.0-cp310-cp310-linux_aarch64.whl"
 
 # -- Torchvision 0.23.0
 wget -O "$TMP/torchvision-0.23.0-cp310-cp310-linux_aarch64.whl" \
-  "https://pypi.jetson-ai-lab.io/jp6/cu126/+f/907/c4c1933789645/torchvision-0.23.0-cp310-cp310-linux_aarch64.whl"
+  "https://pypi.jetson-ai-lab.io/jp6/cu126/+f/907/c4c1933789645/torchvision-0.23.0-cp310-cp310-linux_aarch64.whl#sha256=907c4c1933789645ebb20dd9181d40f8647978e6bd30086ae7b01febb937d2d1"
 python3 -m pip install "$TMP/torchvision-0.23.0-cp310-cp310-linux_aarch64.whl"
 
 
